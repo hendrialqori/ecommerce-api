2a641181cf652e84489a966c21017cc03c1d41f5
Hendri Alqori <teamhendri18@gmail.com>
2025-07-30 21:01:22 +0700

    feat: add QueryParams model

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,12 @@
 package model
 
 type WebResponse[T any] struct {
-	Data T `json:"data"`
+	Data     T            `json:"data"`
+	Metadata *QueryParams `json:"metadata,omitempty"`
+}
+
+type QueryParams struct {
+	Page  int     `json:"page"`
+	Limit int     `json:"limit"`
+	Nama  *string `json:"nama,omitempty"`
 }
